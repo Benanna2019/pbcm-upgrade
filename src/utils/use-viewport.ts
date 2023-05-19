7daf0f5e973bab6b1511f69d35d2ba0bebaf6ee2
Benanna2019 <bass41992ben@gmail.com>
2023-05-19 15:55:30 -0400

    added better checks and defaults for viewport hook

--- src/utils/use-viewport.ts
+++ src/utils/use-viewport.ts
@@ -2,21 +2,25 @@
 import * as React from 'react'
 
 export const useViewport = () => {
-  const [width, setWidth] = React.useState(window?.innerWidth)
-  // Add a second state variable "height" and default it to the current window height
-  const [height, setHeight] = React.useState(window?.innerHeight)
+  if (window) {
+    const [width, setWidth] = React.useState(window.innerWidth)
+    // Add a second state variable "height" and default it to the current window height
+    const [height, setHeight] = React.useState(window.innerHeight)
 
-  React.useEffect(() => {
-    const handleWindowResize = () => {
-      setWidth(window?.innerWidth)
-      // Set the height in state as well as the width
-      setHeight(window?.innerHeight)
-    }
+    React.useEffect(() => {
+      const handleWindowResize = () => {
+        setWidth(window.innerWidth)
+        // Set the height in state as well as the width
+        setHeight(window.innerHeight)
+      }
 
-    window.addEventListener('resize', handleWindowResize)
-    return () => window.removeEventListener('resize', handleWindowResize)
-  }, [])
+      window.addEventListener('resize', handleWindowResize)
+      return () => window.removeEventListener('resize', handleWindowResize)
+    }, [])
 
-  // Return both the height and width
-  return { width, height }
+    // Return both the height and width
+    return { width, height }
+  } else {
+    return { width: 0, height: 0 }
+  }
 }
